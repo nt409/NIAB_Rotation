22f5e897c06a6dd06119aa632c5ffb3af45e91be
nt409 <nt409@cam.ac.uk>
2019-01-24 16:18:58 +0000

    Including manual accuracy test
    Manual accuracy not identical since image tester drops a pixel and then resizes so images not identical

--- Fruit_model/Run/Fruit_model_run_with_my_data.R
+++ Fruit_model/Run/Fruit_model_run_with_my_data.R
@@ -13,24 +13,37 @@ library(tidyr)
 
 ############################################
 # fit model
-model_name <- "fruit_model.h5"
+model_name <- "fruit_model_new.h5" # "fruit_model.h5"
 model_my_own <-create_fruit_model(Train_data_reshaped[,,,channel_no,drop = F],Train_labels,Test_data_reshaped[,,,channel_no,drop = F],Test_labels,channels) # drop = F stops R collapsing array to 3 dimensions not 4
 model_my_own %>% summary()
 model_my_own %>% save_model_hdf5(model_name)
 
-results <- model_my_own %>% predict(Test_data_reshaped[,,,channel_no,drop = F]) # drop = F stops R collapsing array to 3 dimensions not 4
 
+# below model has all fruits
+#new_model <- load_model_hdf5("fruit_model.h5")
+#new_model %>% summary()
+
+results <- model_my_own %>% evaluate(Test_data_reshaped[165:3622,,,channel_no,drop=F], Test_labels[165:3622])
+results
+
+results <- model_my_own %>% predict_classes(Test_data_reshaped[1:164,,,channel_no,drop = F]) # drop = F stops R collapsing array to 3 dimensions not 4
 ############################################
-
-data <- image_tester(internet_path,"Mixed")
+data <- image_tester(internet_path,"Walnut")
 res2 <- model_my_own %>% predict(data)
 preds(res2)
 multipreds(res2,number_probs)
 
-data <- image_tester(pathname,"Test/Mango")
-res2 <- model_my_own %>% predict(data)
-preds(res2)
 
-data <- image_tester(pathname,"Test/Avocado")
+# manually test accuracy
+right <- 0
+wrong <- 0
+for(k in 20:40){
+data <- image_tester(pathname,paste("Test",class_names[k],sep="/"))
 res2 <- model_my_own %>% predict(data)
-preds(res2)
+print(k)
+print(preds(res2)[,3])
+vec<-preds(res2)[,3]-rep(k,length(preds(res2)[,3]))
+right<-right+sum(vec==0)
+wrong<-wrong+(length(preds(res2)[,3]) - sum(vec==0))
+print(paste("Number right =",right,", Number wrong =",wrong,", Accuracy =",right/(wrong+right),sep=''))
+}
--- Fruit_model/Run/Parameters.R
+++ Fruit_model/Run/Parameters.R
@@ -13,13 +13,13 @@ fruit_list <- c("Apple_Braeburn","Banana","Orange","Grape_White","Kiwi")
 class_names_frame <- cbind(class_names,seq(length(class_names)))
 class_names_frame <- as.data.frame(class_names_frame,stringsAsFactors=F)
 colnames(class_names_frame) <- c('Fruit','label')
-labels_to_be_tested <- as.numeric(filter(class_names_frame,Fruit %in% fruit_list)[,2]) # gives the ones specified in fruit list
-# labels_to_be_tested <- c(35,78)
+# labels_to_be_tested <- as.numeric(filter(class_names_frame,Fruit %in% fruit_list)[,2]) # gives the ones specified in fruit list
+labels_to_be_tested <- 20:40
 #labels_to_be_tested <- 1:(length(class_names))
 ############################################
 # model parameters
 channels <- 1 # how many channels
-channel_no <- 2 # which of these channels?
+channel_no <- 3 # which of these channels?
 My_filter_number<- 32
 My_kernel_size  <- c(3,3)
 My_batch_size <- 32
