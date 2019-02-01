279b0d9d07127fda2eba70c24b9f6a7f2011f523
nt409 <nt409@cam.ac.uk>
2019-02-01 14:34:23 +0000

    master model working now

--- Fruit_model/Bigger model/Master_model.R
+++ Fruit_model/Bigger model/Master_model.R
@@ -1,45 +1,106 @@
 library(foreign)
 library(nnet)
 library(stargazer)
+# library(MNP)
+library(dplyr)
 
 
-image_1 <- c('d1_score'=0.1,'d2_score'=0.2,'d3_score'=0.1,'d4_score'=0.2,'d5_score'=0.4,'location'="East_Anglia",'weather'="wet",'crop_variety'="wheat_breed_1",'soil_type'="clay",'disease'="d1")
-data<- as.data.frame(image_1)
-data<-t(data)
-data<-as.data.frame(data)
-
+## d1 - clay soil only, preference for wet, Midlands
+q<-rnorm(1,mean=0,sd=0.1)
+data<- as.data.frame(t(c('d1_score'=0.5+q,'d2_score'=0.2-q,'d3_score'=0.1,'d4_score'=0.1,'d5_score'=0.1,'location'="East_Anglia",'weather'="wet",'crop_variety'="wheat_breed_1",'soil_type'="clay",'disease'="d1")))
 dis_data2<-data
-for(i in 1:10){
-  dis_data2<- rbind(dis_data2,c(0.5+0.01*i,0.3-0.01*i,0.2,0,0,"East_Anglia","wet","wheat_breed_1","clay","d1"))
+
+for(i in 1:200){
+  q<-rnorm(1,mean=0,sd=0.1)
+  w<-rnorm(1,mean=0,sd=1)
+  l<-rnorm(1,mean=0,sd=1)
+  s<-rnorm(1,mean=0,sd=1)
+  weather<-'dry'
+  location<-'East_Anglia'
+  if(w>-0.3){weather='wet'}
+  if(l>-0.1){location='Midlands'}
+  dis_data2<-rbind(dis_data2,t(c('d1_score'=0.5+q,'d2_score'=0.3-q,'d3_score'=0.2,'d4_score'=0.1,'d5_score'=0.1,'location'=location,'weather'=weather,'crop_variety'="wheat_breed_1",'soil_type'="clay",'disease'="d1")))
 }
 
-for(i in 1:10){
-dis_data2<- rbind(dis_data2,c(0.3-0.01*i,0.5+0.01*i,0.2,0,0,"East_Anglia","dry","wheat_breed_1","sandy","d2"))
+## d2 - occurs in both locations, both weathers, both soil types but with preferences
+## d2 preference for sandy, midlands, wet
+for(i in 1:200){
+  q<-rnorm(1,mean=0,sd=0.1)
+  w<-rnorm(1,mean=0,sd=1)
+  l<-rnorm(1,mean=0,sd=1)
+  s<-rnorm(1,mean=0,sd=1)
+  weather<-'dry'
+  location<-'East_Anglia'
+  soil<-'clay'
+  if(w>-0.1){weather='wet'}
+  if(l>-0.5){location='Midlands'}
+  if(s>-0.4){soil='sandy'}
+  dis_data2<-rbind(dis_data2,t(c('d1_score'=0.3-q,'d2_score'=0.5+q,'d3_score'=0.2,'d4_score'=0.1,'d5_score'=0.1,'location'=location,'weather'=weather,'crop_variety'="wheat_breed_1",'soil_type'=soil,'disease'="d2")))
 }
 
-for(i in 1:10){
-  dis_data2<- rbind(dis_data2,c(0.3-0.01*i,0.2,0.5+0.01*i,0,0,"Midlands","wet","wheat_breed_1","clay","d3"))
+## d3 - occurs in both locations, both weathers, both soil types but with preferences
+# d3 preference for dry, EA, clay
+for(i in 1:200){
+  q<-rnorm(1,mean=0,sd=0.1)
+  w<-rnorm(1,mean=0,sd=1)
+  l<-rnorm(1,mean=0,sd=1)
+  s<-rnorm(1,mean=0,sd=1)
+  weather<-'dry'
+  location<-'East_Anglia'
+  soil<-'clay'
+  if(w>0.2){weather='wet'}
+  if(l>0.3){location='Midlands'}
+  if(s>0.5){soil='sandy'}
+  dis_data2<-rbind(dis_data2,t(c('d1_score'=0.3-q,'d2_score'=0.05,'d3_score'=0.55+q,'d4_score'=0.05,'d5_score'=0.05,'location'=location,'weather'=weather,'crop_variety'="wheat_breed_1",'soil_type'=soil,'disease'="d3")))
 }
+dis_data2
+
+dis_data2$d1_score <- as.numeric(as.character(dis_data2$d1_score))
+dis_data2$d2_score <- as.numeric(as.character(dis_data2$d2_score))
+dis_data2$d3_score <- as.numeric(as.character(dis_data2$d3_score))
+dis_data2$d4_score <- as.numeric(as.character(dis_data2$d4_score))
+dis_data2$d5_score <- as.numeric(as.character(dis_data2$d5_score))
 
 # disease_data <- data
-multi <- multinom(disease ~ d1_score + d2_score + d3_score + location + weather + soil_type, data=dis_data)
+multi <- multinom(disease ~ d1_score + d2_score + d3_score + location + weather + soil_type, data=dis_data2) # but isn't treating d_score as continuous
 summary(multi)
-stargazer(multi, type="text", out="multi1.htm")
+stargazer(multi, type="text", out="multi1.htm") # how many more times likely, for a unit change in one isolated variable. Also gives significance
 multi.rrr <- exp(coef(multi))
 multi.rrr
-i=0
-dis<- c('d1_score'=0.1,'d2_score'=0.2,'d3_score'=0.1,'location'="East_Anglia",'weather'="wet",'soil_type'="clay")
+
+# predict for new data
+## d1 - clay soil only, preference for wet, Midlands
+## d2 preference for sandy, Midlands, wet
+## d3 preference for dry, EA, clay
+
+# works - even if looks like d1, if it's sandy will predict next best.
+weather<-'wet'
+soil<-'clay'
+location<-'Midlands'
+d1sc<-0.1
+d2sc<-0.9
+d3sc<-0.2
+
+dis<- c('d1_score'=d1sc,'d2_score'=d2sc,'d3_score'=d3sc,'location'=location,'weather'=weather,'soil_type'=soil)
 dis<-t(dis)
 dis<-as.data.frame(dis)
-predict(multi,dis)
+dis$d1_score <- as.numeric(as.character(dis$d1_score))
+dis$d2_score <- as.numeric(as.character(dis$d2_score))
+dis$d3_score <- as.numeric(as.character(dis$d3_score))
+
+Probability<-predict(multi,dis,"probs")
+pred<-as.data.frame(Probability)
+pred$Disease <- c(1,2,3)
+frame3<-pred[order(-pred$Probability),]
+frame3
+
+# filter(dis_data2,disease=="d1")
 
 
-
-# 
+ 
 # disease_model.lm<-lm(disease~disease_image_score+location+weather+date+crop_variety+soil_type, # + interactions?? +treatments+irrigation
 #                                   data=disease_data)
 # summary(disease_model.lm)
-
 
 # AIC: . Models with delta_AIC < 2 have substantial evidence, 2 < delta_AIC < 7 have considerably
 # less support, and delta_AIC > 10 means that a model has essentially no support
@@ -47,6 +108,6 @@ predict(multi,dis)
 # options(na.action = "na.fail") # we need to set this option for dredge to work
 # disease.dredge <- dredge(disease_model.lm)
 # disease.dredge
-# 
+
 # # importance: As a rule of thumb, a variable with score >0.8 is highly important, between 0.8 and 0.5 is moderately important, and <0.5 is not important
 # importance(disease.dredge)
---
+++ Fruit_model/data_analysis_iris.R
@@ -0,0 +1,227 @@
+
+library(data.table)
+library(psych)
+library(outliers)
+library(glmnet)
+library(rminer)
+library(dplyr)
+library(e1071)
+
+build_model <- function() {
+  
+  model <- keras_model_sequential() %>%
+    layer_dense(units = 70, activation = "relu",
+                input_shape = dim(sdata)[2]) %>%
+    layer_dense(units = 70, activation = "relu") %>%
+    layer_dense(units = 1)
+  
+  model %>% compile(
+    loss = "mse",
+    optimizer = optimizer_rmsprop(),
+    metrics = list("mean_absolute_error")
+  )
+  
+  model
+}
+
+
+
+tune_svm <- function(sdata, f){
+  
+  tc <- tune.control(cross = 5)
+  tuned_par <- tune.svm(f, data = sdata, 
+                        gamma = 10^(-5:-1), cost = seq(1,5, by = 0.5),
+                        epsilon = seq(0, 0.2, by = 0.01), tunecontrol = tc)
+  return(tuned_par)
+}
+
+#' run SVM/regression
+#' @param train_data train dataset
+#' @param test_data test dataset
+#' @param cn class name
+#' @param dl descriptor list
+#' @param p percentage split
+#' @param f formula
+run_svm <- function(train_data, test_data, cn, dl, f, tuned_par){
+  
+ 
+ 
+  svm_model <- svm(f, train_data, gamma = tuned_par$gamma, 
+                   epsilon = tuned_par$epsilon, 
+                   cost=tuned_par$cost, kernel = 'radial')
+  
+  
+  predictYsvm <- predict(svm_model, test_data[, -ncol(test_data), 
+                                              with = FALSE])
+  
+  plot(test_data[[cn]], predictYsvm, col = "blue", pch=4,  main = 'SVM',
+       ylab= 'predicted', xlab = 'observed')
+  res <- caret::postResample(as.numeric(test_data[[cn]]), predictYsvm)
+  print(res)
+  
+  
+  plot(test_data[[cn]], col='red', pch=19); abline(h=0,v=0,lty=3)
+  points(predictYsvm,col="blue",cex=1) # show the support vectors
+  
+  
+  return(predictYsvm)
+}
+
+#' search_outliers
+#' @param sdata dataset
+#' @param dl descriptor list
+plot_data <- function(sdata, descriptor_list){
+  
+  for(descriptor in descriptor_list){
+    
+    plot(sdata[[descriptor]])
+    if(readline(descriptor) == 'q') { break}
+    
+  }
+}
+
+#' search_outliers
+#' @param sdata dataset
+#' @param dl descriptor list
+
+search_outliers <- function(sdata, dl){
+  
+  o_list <- list()
+  for(descriptor in dl){
+    o = match(outlier(sdata[[descriptor]]), sdata[[descriptor]])
+    sdata[[descriptor]][o] = NA
+    #print(o)
+  }
+  return(sdata)
+  
+}
+
+#' run run_LASSO
+#' @param train_data train dataset
+#' @param test_data test dataset
+#' @param cn class name
+#' @param dl descriptor list
+
+run_LASSO <- function(train_data, test_data, cn, dl){
+  
+
+  lambda <- 10^seq(10, -2, length = 100)
+  cv.out <- cv.glmnet(as.matrix(train_data[,-ncol(train_data), with=FALSE]), 
+                      as.matrix(train_data[,ncol(train_data), with=FALSE]), 
+                      alpha = 0)
+  
+  
+  lasso.mod <- glmnet(as.matrix(train_data[,-ncol(train_data), with=FALSE]), 
+                      as.matrix(train_data[,ncol(train_data), with=FALSE]), 
+                      alpha = 1, lambda = lambda)
+  
+ 
+  bestlam <- cv.out$lambda.min
+  n <- ncol(test_data)
+  lasso.pred <- predict(lasso.mod, s = bestlam, 
+                        newx = as.matrix(test_data[,n-1, with=FALSE]), 
+                        type="response")
+  plot(test_data[[class_name]], lasso.pred , col = "blue", pch=4,
+       main = 'LASSO')
+  res <- caret::postResample(as.numeric(test_data[[class_name]]), lasso.pred)
+  print(res)
+  return(lasso.pred)
+  
+}
+
+
+feature_selection <- function(train_data, model_name = "xgboost" ){
+  
+  
+  M <- fit(f, data = train_data, model=model_name)
+  I <- Importance(M, data=train_data)
+  i <- which(I$imp > 0)
+  pdf('features_pdf.pdf')
+  par(mfrow = c(3,5))
+  for(x in i){
+    vecplot(I,graph="VEC", xval=x,
+            main=paste(colnames(train_data)[x], 'vs resp'),Grid=10)
+    if(readline(x) == 'q') { break}
+  }
+  
+  dev.off()
+  L=list(runs=1,sen=t(I$imp),sresponses=I$sresponses)
+  mgraph(L,graph="IMP",leg=names(train_data),col="gray",Grid=10)
+}
+
+
+
+run_gboosting <- function(train_data, test_data, class_name){
+  
+  n <- ncol(train_data)
+  train.y <- as.matrix(train_data[,n, with = FALSE])
+  #i <- ifelse(train_data[[class_name]] > 1, 0.5, 0.8)
+  
+  xgmodel <- xgboost(data = data.matrix(train_data[,-n, with = FALSE]), 
+                     label = train.y, 
+                     objective = "reg:linear",
+                     booster = 'gblinear',
+                     max.depth = 2, 
+                     eta = 1, 
+                     nthread = 2, 
+                     nround = 2, 
+                     min_child_weight = 1, 
+                     subsample = 0.5, 
+                     colsample_bytree = 1, 
+                     num_parallel_tree = 1,
+                     eval_metric = 'rmse'
+                     )
+  
+
+  
+  
+  xgb.pred <- predict(xgmodel, data.matrix(test_data[,-n, with = FALSE]))
+  plot(test_data[[class_name]], xgb.pred , col = "blue", pch=4, main = 'GB')
+  res <- caret::postResample(as.numeric(test_data[[class_name]]), xgb.pred)
+  print(res)
+  
+  
+}
+
+raw_data <- iris
+class_name <- 'Species'
+descriptor_list <- colnames(raw_data)[1:4]
+
+
+
+#post_data <- raw_data[,descriptor_list, with = FALSE]
+#post_data <- setDT(data.frame(scale(post_data)))
+#post_data <- search_outliers(post_data, descriptor_list[-(length(descriptor_list))])
+
+post_data <- setDT(raw_data[complete.cases(raw_data),]) 
+
+correl <- cor(post_data[, 1:4])
+co <- 0.3
+
+# Formula
+f <- as.formula(paste(class_name, '~ .'))
+
+set.seed(42)
+p <- 70
+index <- sample(1:nrow(post_data), p*nrow(post_data)/100)
+train_data <- post_data[index, c(descriptor_list, class_name), 
+                        with = FALSE]
+test_data  <- post_data[-index, c(descriptor_list, class_name), 
+                        with = FALSE]
+
+# Search the best parameters
+o <- tune_svm(train_data, f)
+
+tuned_par <- list()
+tuned_par$gamma <- 0.01#o$best.parameters$gamma 
+tuned_par$epsilon <- 0.04#o$best.parameters$epsilon
+tuned_par$cost <- 2.5#o$best.parameters$cost
+par(mfrow = c(3,1))
+resSVM <- run_svm(train_data, test_data, class_name, 
+                  descriptor_list, f, 
+                  tuned_par)
+resLASSO <-  run_LASSO(train_data, test_data, class_name, 
+                       descriptor_list)
+resGB <- run_gboosting(train_data, test_data, class_name)
+
+
