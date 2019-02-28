ccf9bf365c2c070bf2ee20c27f6ac4138b4d32dd
nt409 <nt409@cam.ac.uk>
2019-02-28 14:10:00 +0000

    Added in bit of output analysis

--- Fruit_model/Anyela_dropbox/tests_v1.R
+++ Fruit_model/Anyela_dropbox/tests_v1.R
@@ -31,7 +31,6 @@ imageinfo <- create_image_container(annotations)
 # Scale bbox
 imageinfo <- scale_image_boundingbox(imageinfo, params$target_height, params$target_width)
 
-#iimagesc <- select_scale_image(imageinfo, "scaled")
 
 n_samples <- nrow(imageinfo)
 train_indices <- sample(1:n_samples, params$proportion_of_samples * n_samples)
@@ -41,9 +40,6 @@ validation_data <- imageinfo[-train_indices,]
 
 # Data generator
 image_size <- params$target_width # same as height
-# threshold <- 0.4
-#n_classes <- 2
-# class_background <- 21
 
 
 feature_extractor <- application_xception(
@@ -61,10 +57,11 @@ common <- feature_extractor$output %>%
   #layer_batch_normalization() %>% #turn off??
   layer_dropout(rate = 0.5)
 
+# Bounding box
 regression_output <-
-  layer_dense(common, units = 4, name = "regression_output") # Bounding box
+  layer_dense(common, units = 4, name = "regression_output")
 
-
+# Class prediction
 class_output <- layer_dense(
   common,
   units = params$cl_output, # was 20
@@ -77,8 +74,8 @@ model <- keras_model(
   outputs = list(regression_output, class_output)
 )
 
+# box metric
 metric_iou <- function(y_true, y_pred) {
-  
   # order is [x_left, y_top, x_right, y_bottom]
   intersection_xmin <- k_maximum(y_true[ ,1], y_pred[ ,1])
   intersection_ymin <- k_maximum(y_true[ ,2], y_pred[ ,2])
@@ -184,7 +181,11 @@ model %>% fit_generator(
 
 
 
- train_1_8 <- train_data[1:8, c("file_name",
+
+
+# analyse output
+
+train_1_8 <- train_data[1:8, c("file_name",
                                 "name",
                                 "x_left_scaled",
                                 "y_top_scaled",
@@ -192,22 +193,59 @@ model %>% fit_generator(
                                 "y_bottom_scaled")]
  
 
-k <- 8 #4
-for (i in k:k) {
+preds<-  model %>% predict(
+  load_and_preprocess_image(train_1_8[1, "file_name"], 
+                            params$target_height, params$target_width),
+  batch_size = 1
+)
+plot_image_with_boxes_single(train_1_8$file_name[i],
+                             train_1_8$name[i],
+                             train_1_8[i, 3:6] %>% as.matrix(),
+                             scaled = TRUE, # FALSE?
+                             box_pred = preds[[1]], # should be just preds[[1]]
+                             class_pred = preds[[2]]
+)
+preds[[1]]
+preds[[2]]
+box_predictions<-as.data.frame(preds[[1]])
+class_preds <- as.data.frame(preds[[2]])
+for (i in 2:8) {
   preds <-
     model %>% predict(
       load_and_preprocess_image(train_1_8[i, "file_name"], 
                                 params$target_height, params$target_width),
       batch_size = 1
     )
-  plot_image_with_boxes_single(train_1_8$file_name[i],
-                        train_1_8$name[i],
-                        train_1_8[i, 3:6] %>% as.matrix(),
-                        scaled = TRUE, # FALSE?
-                        box_pred = preds[[1]], # should be just preds[[1]]
-                        class_pred = preds[[2]]
-                        )
+preds2<-as.data.frame(preds[[1]])
+cl_preds2<-as.data.frame(preds[[2]])
+box_predictions<- rbind(box_predictions,preds2)
+class_preds <- rbind(class_preds,cl_preds2)
 }
-preds[[1]]
-train_1_8[i, 3:6]
-preds[[2]]
+
+train_1_8[1:8, 3:6]
+box_predictions
+colnames(box_predictions)<-c("xl_pred","yt_pred","xr_pred","yb_pred")
+class_preds1<-class_preds
+colnames(class_preds) <- c('MSD','BS')
+data_labels<-as.data.frame(train_1_8$name[1:8])
+colnames(data_labels)<-"label"
+
+class_preds$predicted_disease <- names(class_preds)[apply(class_preds, 1, which.max)]
+class_preds<-cbind(class_preds,data_labels)
+class_preds
+corners<-cbind(train_1_8[1:8, 3:6],box_predictions)
+corners$xl_error <- corners[,1]-corners[,5]
+corners$yt_error <- corners[,2]-corners[,6]
+corners$xr_error <- corners[,3]-corners[,7]
+corners$yb_error <- corners[,4]-corners[,8]
+
+
+
+i<-8
+plot_image_with_boxes_single(train_1_8$file_name[i],
+                             train_1_8$name[i],
+                             train_1_8[i, 3:6] %>% as.matrix(),
+                             scaled = TRUE, # FALSE?
+                             box_pred = box_predictions[i,], # should be just preds[[1]]
+                             class_pred = class_preds1[i,]
+)
