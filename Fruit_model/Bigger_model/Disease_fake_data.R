1a233e131e47648e4296ac3179cce380228c3842
nt409 <nt409@cam.ac.uk>
2019-03-01 15:13:50 +0000

    svm working and fake data better structured

--- Fruit_model/Bigger_model/Disease_fake_data.R
+++ Fruit_model/Bigger_model/Disease_fake_data.R
@@ -15,112 +15,116 @@
 # treatments applied (and when),
 # irrigation
 
-# params<- list(
-#   'd1'= list('weather' = "wet",
-#              'location' = "midlands",
-#              'crop_variety' = "var_1",
-#              'soil_type' = "clay"
-#   ),
-#   'd2'= list('weather' = "wet",
-#              'location' = "midlands",
-#              'crop_variety' = "var_1",
-#              'soil_type' = "clay"
-#   ),
-#   'd3'= list('weather' = "wet",
-#              'location' = "midlands",
-#              'crop_variety' = "var_1",
-#              'soil_type' = "clay"
-#   )
-# )
-# params$d1$weather
-
-
-## d1 - clay soil only, preference for wet, Midlands , Wheat Breed 2
-q<-rnorm(1,mean=0,sd=0.2)
-data<- as.data.frame(t(c('disease'="d1",'d1_score'=0.5+q,'d2_score'=0.3-q,'d3_score'=0.2,'location'="East_Anglia",'rainfall'=50,'mean_temp'=16,'crop_variety'="WB1",'soil_type'="clay")))
-dis_data2<-data
-
+# mean values
 d1_av1<-0.9
 d1_av2<-0.05
 d1_av3<-0.05
+
+d2_av1<-0.05
+d2_av2<-0.9
+d2_av3<-0.05
+
+d3_av1<-0.05
+d3_av2<-0.05
+d3_av3<-0.9
+#rainfall av
+r1_av<-40
+r2_av<-50
+r3_av<-60
+#temp av
+t1_av<-16
+t2_av<-18
+t3_av<-14
+
+## d1 - clay soil only, preference for wet, Midlands , Wheat Breed 2
+q<-rnorm(1,mean=0,sd=1)
+data<- as.data.frame(t(c('disease'="d1",'d1_score'=d1_av1+0.2*q,'d2_score'=d1_av2-0.2*q,'d3_score'=d1_av3,'location'="East_Anglia",'rainfall'=50,'mean_temp'=16,'crop_variety'="WB1",'soil_type'="clay")))
+dis_data2<-data
+
 for(i in 2:200){
-  q<-rnorm(1,mean=0,sd=0.05)
+  #initialise random variables
+  q<-rnorm(1,mean=0,sd=1)
   w<-rnorm(1,mean=0,sd=1)
   w2<-rnorm(1,mean=0,sd=1)
   l<-rnorm(1,mean=0,sd=1)
   s<-rnorm(1,mean=0,sd=1)
   t<-rnorm(1,mean=0,sd=1)
+  # data
   location<-'East_Anglia'
   soil<-"clay"
   WB <- 'WB1'
-  rainfall <- 40+10*w
-  mean_temp<-16+w2
-  d1_score<-d1_av1+q
-  d2_score<-d1_av2-q
+  rainfall <- r1_av+10*w
+  mean_temp<-t1_av+w2
+  d1_score<-d1_av1+0.2*q
+  d2_score<-d1_av2-0.2*q
   d3_score<-d1_av3
+  # adjustments
   if(l>-0.1){location='Midlands'}
   if(t>-0.1){WB='WB2'}
-  if(q>0.025){d1_score<-0.05
-  d2_score<-1
-  d3_score<-0.05} # gets it completely wrong in this case
+  if(q>0.1){d1_score<-0.05
+  d2_score<-0.9
+  d3_score<-0.05} # image classifier gets it completely wrong in this case
+  # add into data frame
   dis_data2<-rbind(dis_data2,t(c('disease'="d1",'d1_score'=d1_score,'d2_score'=d2_score,'d3_score'=d3_score,'location'=location,'rainfall'=rainfall,'mean_temp'=mean_temp,'crop_variety'=WB,'soil_type'=soil)))
 }
 
 ## d2 - occurs in both locations, both weathers, both soil types but with preferences
 ## d2 preference for sandy, midlands, wet, WB2
-d2_av1<-0.05
-d2_av2<-0.9
-d2_av3<-0.05
 for(i in 1:200){
-  q<-rnorm(1,mean=0,sd=0.05)
+  #initialise random variables
+  q<-rnorm(1,mean=0,sd=1)
   w<-rnorm(1,mean=0,sd=1)
   w2<-rnorm(1,mean=0,sd=1)
   l<-rnorm(1,mean=0,sd=1)
   s<-rnorm(1,mean=0,sd=1)
   t<-rnorm(1,mean=0,sd=1)
+  # data
   location<-'East_Anglia'
   soil<-'clay'
   WB <- 'WB1'
-  rainfall <- 50+10*w
-  mean_temp<-18+w2
-  d1_score<-d2_av1-q
-  d2_score<-d2_av2+q
+  rainfall <- r2_av+10*w
+  mean_temp<-t2_av+w2
+  d1_score<-d2_av1-0.2*q
+  d2_score<-d2_av2+0.2*q
   d3_score<-d2_av3
+  # adjustments
   if(l>-0.5){location='Midlands'}
   if(s>-0.4){soil='sandy'}
   if(t>-0.5){WB='WB2'}
-  if(q>0.025){d1_score<-0.05
+  if(q>0.05){d1_score<-0.05
   d2_score<-0.05
-  d3_score<-0.9} # gets it completely wrong in this case
+  d3_score<-0.9} # image classifier gets it completely wrong in this case
+  # add into data frame
   dis_data2<-rbind(dis_data2,t(c('disease'="d2",'d1_score'=d1_score,'d2_score'=d2_score,'d3_score'=d3_score,'location'=location,'rainfall'=rainfall,'mean_temp'=mean_temp,'crop_variety'=WB,'soil_type'=soil)))
 }
 
 ## d3 - occurs in both locations, both soil types but with preferences
 # d3 preference for dry, EA, clay, WB1
-d3_av1<-0.05
-d3_av2<-0.05
-d3_av3<-0.9
 for(i in 1:200){
-  q<-rnorm(1,mean=0,sd=0.2)
+  #initialise random variables
+  q<-rnorm(1,mean=0,sd=1)
   w<-rnorm(1,mean=0,sd=1)
   w2<-rnorm(1,mean=0,sd=1)
   l<-rnorm(1,mean=0,sd=1)
   s<-rnorm(1,mean=0,sd=1)
   t<-rnorm(1,mean=0,sd=1)
+  # data
   location<-'East_Anglia'
   soil<-'clay'
   WB <- 'WB1'
-  rainfall <- 60+12*w
-  mean_temp<-14+w2
-  d1_score<-d3_av1-q
+  rainfall <- r3_av+10*w
+  mean_temp<-t3_av+w2
+  d1_score<-d3_av1-0.2*q
   d2_score<-d3_av2
-  d3_score<-d3_av3+q
+  d3_score<-d3_av3+0.2*q
+  # adjustments
   if(l>0.3){location='Midlands'}
   if(s>0.5){soil='sandy'}
   if(t>0.5){WB='WB2'}
-  if(q>0.01){d1_score<-0.9
+  if(q>0.05){d1_score<-0.9
   d2_score<-0.05
-  d3_score<-0.05} # gets it completely wrong in this case
+  d3_score<-0.05} # image classifier gets it completely wrong in this case
+  # add into data frame
   dis_data2<-rbind(dis_data2,t(c('disease'="d3",'d1_score'=d1_score,'d2_score'=d2_score,'d3_score'=d3_score,'location'=location,'rainfall'=rainfall,'mean_temp'=mean_temp,'crop_variety'=WB,'soil_type'=soil)))
 }
 dis_data2
@@ -131,6 +135,7 @@ dis_data2$d3_score <- as.numeric(as.character(dis_data2$d3_score))
 dis_data2$rainfall <- as.numeric(as.character(dis_data2$rainfall))
 dis_data2$mean_temp <- as.numeric(as.character(dis_data2$mean_temp))
 
+# make categorical data into numeric data taking values 0 or 1
 dis_data2 <- mutate(dis_data2,
                     Loc_EA_indic = ifelse(location=="East_Anglia",1,0),
                     Loc_Midlands_indic = ifelse(location=="Midlands",1,0),
@@ -139,3 +144,5 @@ dis_data2 <- mutate(dis_data2,
                     ST_clay_indic = ifelse(soil_type=="clay",1,0),
                     ST_sandy_indic = ifelse(soil_type=="sandy",1,0)
                     )
+
+head(dis_data2)
--- Fruit_model/Bigger_model/SVM_attempt.R
+++ Fruit_model/Bigger_model/SVM_attempt.R
@@ -2,100 +2,81 @@ library(e1071)
 # attach(iris)
 
 source('~/GitHub/NIAB_Rotation/Fruit_model/Bigger_model/Disease_fake_data.R')
-
 # do we need prevalence data??
 
-data <- dis_data2
+################################################
+#fn to create svm model, and a tuned svm model. Could improve tuning.
+svm_creator<-function(data_to_use,predictor_data_to_use){
+  svm_model_within_function <- svm(category_id~., data=data_to_use)
+  summary(svm_model_within_function)
+  
+  pred_within_function <- predict(svm_model_within_function,predictor_data_to_use)
+  
+  svm_tune_within_function <- tune(svm, train.x=predictor_data_to_use, train.y=class_labels,
+                                   kernel="radial", ranges=list(cost=10^(-1:2), gamma=c(.5,1,2)))
+  
+  print(svm_tune_within_function)
+  
+  ##
+  resulting_cost_within_function<-as.numeric(svm_tune_within_function$best.parameters[1]) # get from svm_tune
+  resulting_gamma_within_function<-as.numeric(svm_tune_within_function$best.parameters[2]) # get from svm_tune
+  ##
+  
+  svm_model_after_tune_within_function <- svm(category_id ~ ., data=data_to_use, kernel="radial", cost=resulting_cost_within_function, gamma=resulting_gamma_within_function)
+  summary(svm_model_after_tune_within_function)
+  
+  pred_within_function_tuned <- predict(svm_model_after_tune_within_function,predictor_data_to_use)
+  return(list('svm'=svm_model_within_function,'svm_tuned'=svm_model_after_tune_within_function,'pred'=pred_within_function,'pred_tuned'=pred_within_function_tuned,'tune'=svm_tune_within_function))
+}
+################################################
+
+data <- dis_data2 # comes in from Disease_fake_data.R
 category_id<-dis_data2$disease
 names(data)[names(data)=="disease"] <- "category_id"
 #########################################################
-raw_data <- subset(data, select = c(category_id,location,rainfall,mean_temp,crop_variety,soil_type,d1_score,d2_score,d3_score))
-predictor_data <- subset(data, select = c(-location,-crop_variety,-soil_type,-category_id))
+raw_data <- subset(data, select = c(category_id,d1_score,d2_score,d3_score,location,rainfall,mean_temp,crop_variety,soil_type))
 data_use<- subset(data, select = c(-location,-crop_variety,-soil_type))
+predictor_data <- subset(data_use, select = c(-category_id))
 class_labels <- category_id
 
 head(data_use)
-###
+################################################
+svm_all<-svm_creator(data_use,predictor_data)
 
-svm_model <- svm(category_id~., data=data_use)
-summary(svm_model)
-
-pred <- predict(svm_model,predictor_data)
-table(pred,class_labels)
-
-svm_tune <- tune(svm, train.x=predictor_data, train.y=class_labels,
-                 kernel="radial", ranges=list(cost=10^(-1:2), gamma=c(.5,1,2)))
-
-print(svm_tune)
-
-##
-resulting_cost<-as.numeric(svm_tune$best.parameters[1]) # get from svm_tune
-resulting_gamma<-as.numeric(svm_tune$best.parameters[2]) # get from svm_tune
-##
-
-svm_model_after_tune <- svm(category_id ~ ., data=data_use, kernel="radial", cost=resulting_cost, gamma=resulting_gamma)
-summary(svm_model_after_tune)
-
-pred_tuned <- predict(svm_model_after_tune,predictor_data)
-table(pred_tuned,class_labels)
+# table(svm_all$pred,class_labels) # confusion matrices
+# table(svm_all$pred_tuned,class_labels) # confusion matrices
 
 ################################################
 # without disease images
-
 data_use_without_images<- subset(data, select = c(-location,-crop_variety,-soil_type,-d1_score,-d2_score,-d3_score))
-predictor_data_without_images <- subset(data, select = c(-location,-crop_variety,-soil_type,-category_id,-d1_score,-d2_score,-d3_score))
+predictor_data_without_images <- subset(data_use_without_images, select = c(-category_id))
 
-svm_model_without_images <- svm(category_id~., data=data_use_without_images)
-summary(svm_model_without_images)
+svm_no_images<-svm_creator(data_use_without_images,predictor_data_without_images)
 
-pred_without_images <- predict(svm_model_without_images,predictor_data_without_images)
-table(pred_without_images,class_labels)
-
-svm_tune_without_images <- tune(svm, train.x=predictor_data_without_images, train.y=class_labels,
-                 kernel="radial", ranges=list(cost=10^(-1:2), gamma=c(.5,1,2)))
-
-print(svm_tune_without_images)
-
-##
-resulting_cost_without_images<-as.numeric(svm_tune_without_images$best.parameters[1]) # get from svm_tune
-resulting_gamma_without_images<-as.numeric(svm_tune_without_images$best.parameters[2]) # get from svm_tune
-##
-
-svm_model_after_tune_without_images <- svm(category_id ~ ., data=data_use_without_images, kernel="radial", cost=resulting_cost_without_images, gamma=resulting_gamma_without_images)
-summary(svm_model_after_tune_without_images)
-
-pred_without_images_tuned <- predict(svm_model_after_tune_without_images,predictor_data_without_images)
-table(pred_without_images_tuned,class_labels)
+# table(svm_no_images$pred,class_labels) # confusion matrices
+# table(svm_no_images$pred_tuned,class_labels) # confusion matrices
 
 ################################################
 # only with images
-
 data_use_images_only<- subset(data, select = c(category_id,d1_score,d2_score,d3_score))
-predictor_data_images_only <- subset(data, select = c(d1_score,d2_score,d3_score))
+predictor_data_images_only <- subset(data_use_images_only, select = c(-category_id))
+
+svm_im_only<-svm_creator(data_use_images_only,predictor_data_images_only)
+
+# table(svm_im_only$pred,class_labels) # confusion matrices
+# table(svm_im_only$pred_tuned,class_labels) # confusion matrices
+
+################################################
+# comparison:
+table(svm_all$pred,class_labels)
+table(svm_no_images$pred,class_labels)
+table(svm_im_only$pred,class_labels)
+
+table(svm_all$pred_tuned,class_labels)
+table(svm_no_images$pred_tuned,class_labels)
+table(svm_im_only$pred_tuned,class_labels)
 
 
-#function(data_to_use,)
-svm_model_images_only <- svm(category_id~., data=data_use_images_only)
-summary(svm_model_images_only)
-
-pred_images_only <- predict(svm_model_images_only,predictor_data_images_only)
-table(pred_images_only,class_labels)
-
-svm_tune_images_only <- tune(svm, train.x=predictor_data_images_only, train.y=class_labels,
-                                kernel="radial", ranges=list(cost=10^(-1:2), gamma=c(.5,1,2)))
-
-print(svm_tune_images_only)
-
-##
-resulting_cost_images_only<-as.numeric(svm_tune_images_only$best.parameters[1]) # get from svm_tune
-resulting_gamma_images_only<-as.numeric(svm_tune_images_only$best.parameters[2]) # get from svm_tune
-##
-
-svm_model_after_tune_images_only <- svm(category_id ~ ., data=data_use_images_only, kernel="radial", cost=resulting_cost_images_only, gamma=resulting_gamma_images_only)
-summary(svm_model_after_tune_images_only)
-
-pred_images_only_tuned <- predict(svm_model_after_tune_images_only,predictor_data_images_only)
-table(pred_images_only_tuned,class_labels)
-
-# compare to 
-table(pred_tuned,class_labels)
+svm_all$tune$best.performance
+svm_no_images$tune$best.performance
+svm_im_only$tune$best.performance
